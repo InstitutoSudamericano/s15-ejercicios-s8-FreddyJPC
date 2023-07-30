7c6f856d90a9691a4989a735e207423732007cac
FreddyJPC <freddypaguay20@gmail.com>
2023-07-29 22:41:42 -0500

    s15ejercicios8

--- exercise01.js
+++ exercise01.js
@@ -1,1 +1,16 @@
+// Mostrar la suma elementos impares
+function sumImpares(list){
+    let MULT=2;
+    let sum=0
+    for(let i=0; i<list.length;i++){
+        if ((list[i] % MULT)!=0){
+            sum=sum+list[i];
+        }
+    }
+    return sum;
 
+}
+let nums = [3,5,8,4]
+let result;
+result= sumImpares(nums);
+alert(result)
--- exercise08.js
+++ exercise08.js
@@ -1,1 +1,16 @@
+// Mostrar la suma de los elementos donde el índice sea mayor al elemento
 
+function sumelementindicemayor(list){
+    let sum=0
+    for(let i=0; i<list.length;i++){
+        if (i>list[i]){
+            sum=sum+list[i];
+        }
+    }
+    return sum;
+
+}
+let nums = [0,2,1,1]
+let result;
+result= sumelementindicemayor(nums);
+alert(result)
